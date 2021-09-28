f00cc9ab339be04ae11f77d60c29fe3453e6e46c
Nainesh Rathod <naineshrathod20@gmail.com>
2021-09-28 11:55:42 +0530

    First commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.20;
+
+contract Election{
+    // store candidate
+
+    // read candidate
+    string public candidate;
+    // constructor
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
